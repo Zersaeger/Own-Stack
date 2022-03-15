6e39953af725c518184df69dee99796a7207ea13
Zersaeger <101479976+Zersaeger@users.noreply.github.com>
2022-03-15 21:57:26 +0100

    Projektdateien hinzufügen.

---
+++ Stack/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        
+        Stack.StackL firstStack = new(100000);
+        for(int i = 0; i < 50; i++)
+        {
+            firstStack.Push(i);
+        }
+        firstStack.Clear();
+        firstStack.Push(10);
+        int length = firstStack.Length();
+        Console.WriteLine(length);
+    }
+}
---
+++ Stack/StackL.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    internal class StackL
+    {
+        private int size = 10;
+        private int spaceUsed = 0;
+        private int[] stack;
+        public StackL(int expsize)
+        {
+             size = expsize;
+             stack = new int[size];
+        }
+        public void Push(int value)
+        {
+            stack[spaceUsed] = value;
+            spaceUsed++;
+            if(size == spaceUsed)
+            {
+                size += 10;
+                int[] newStack = new int[size];
+                for(int i = 0; i < stack.Length; i++)
+                {
+                    newStack[i] = stack[i];
+                }
+                stack = newStack;
+            }
+        }
+        public int Pop()
+        {
+            int stackPop;
+            spaceUsed--;
+            stackPop = stack[spaceUsed];
+            return stackPop;          
+        }
+        public void Clear()
+        {
+            spaceUsed = 0;
+        }
+        public int Length()
+        {
+            return spaceUsed;
+        }
+        
+    }
+}
